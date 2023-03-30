ca69c5a020e9e9bf3fac209db96c1c8be7acde41
Something231 <119870385+Something231@users.noreply.github.com>
2023-03-30 19:22:35 +0800

    triagnele

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const input = document.querySelector('input[type="text"]');
 input.addEventListener('input', () => {
   document.body.style.backgroundColor = 'pink';
 });
+const triangle = document.getElementById('triangle');
+document.addEventListener('mousemove', (e) => {
+  triangle.style.left = e.pageX + 'px';
+  triangle.style.top = e.pageY + 'px';
+});

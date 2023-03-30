9beed23ad9cd10c79f811d9e215e3f7a6c52a3fc
Something231 <119870385+Something231@users.noreply.github.com>
2023-03-30 18:54:11 +0800

    added images, updated charamov

--- game/charamov.py
+++ game/charamov.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import pygame
 import sys
 import os
@@ -10,8 +8,8 @@ Variables
 
 worldx = 960
 worldy = 720
-fps = 40
-ani = 4
+fps = 40  # frame rate
+ani = 4  # animation cycles
 world = pygame.display.set_mode([worldx, worldy])
 
 BLUE = (25, 25, 200)
@@ -31,9 +29,6 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.movex = 0
-        self.movey = 0
-        self.frame = 0
         self.images = []
         for i in range(1, 5):
             img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
@@ -42,35 +37,6 @@ class Player(pygame.sprite.Sprite):
             self.images.append(img)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
-
-    def control(self, x, y):
-        """
-        control player movement
-        """
-        self.movex += x
-        self.movey += y
-
-    def update(self):
-        """
-        Update sprite position
-        """
-
-        self.rect.x = self.rect.x + self.movex
-        self.rect.y = self.rect.y + self.movey
-
-        # moving left
-        if self.movex < 0:
-            self.frame += 1
-            if self.frame > 3*ani:
-                self.frame = 0
-            self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
-
-        # moving right
-        if self.movex > 0:
-            self.frame += 1
-            if self.frame > 3*ani:
-                self.frame = 0
-            self.image = self.images[self.frame//ani]
 
 
 '''
@@ -88,7 +54,7 @@ player.rect.x = 0  # go to x
 player.rect.y = 0  # go to y
 player_list = pygame.sprite.Group()
 player_list.add(player)
-steps = 10
+
 
 '''
 Main Loop
@@ -106,25 +72,11 @@ while main:
         if event.type == pygame.KEYDOWN:
             if event.key == ord('q'):
                 pygame.quit()
-                try:
-                    sys.exit()
-                finally:
-                    main = False
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(-steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(steps, 0)
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                print('jump')
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(-steps, 0)
-
+            try:
+                sys.exit()
+            finally:
+                main = False
     world.blit(backdrop, backdropbox)
-    player.update()
     player_list.draw(world)
     pygame.display.flip()
     clock.tick(fps)
